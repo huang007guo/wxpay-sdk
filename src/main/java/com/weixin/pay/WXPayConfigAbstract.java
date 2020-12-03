6edf0684f8135596540df00d5ba6724d35680d01
hank-huangguoliang <526588941@qq.com>
2020-12-03 10:49:26 +0800

    saas init

---
+++ src/main/java/com/weixin/pay/WXPayConfigAbstract.java
@@ -0,0 +1,69 @@
+package com.weixin.pay;
+
+import com.weixin.pay.constants.WXPayConstants;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ * 未实现的配置类
+ *
+ * @author hank
+ */
+public class WXPayConfigAbstract extends WXPayConfig {
+
+    @Override
+    public String getAppID() {
+        return null;
+    }
+
+    @Override
+    public String getMchID() {
+        return null;
+    }
+
+    @Override
+    public String getKey() {
+        return null;
+    }
+
+    @Override
+    public InputStream getCertStream() {
+        return null;
+    }
+
+    @Override
+    public int getHttpConnectTimeoutMs() {
+        return super.getHttpConnectTimeoutMs();
+    }
+
+    @Override
+    public int getHttpReadTimeoutMs() {
+        return super.getHttpReadTimeoutMs();
+    }
+
+    @Override
+    WXPayDomain getWXPayDomain() {
+        return null;
+    }
+
+    public String getPrimaryDomain() {
+        return "api.mch.weixin.qq.com";
+    }
+
+    public String getAlternateDomain() {
+        return "api2.mch.weixin.qq.com";
+    }
+
+    @Override
+    public int getReportWorkerNum() {
+        return 1;
+    }
+
+    @Override
+    public int getReportBatchSize() {
+        return 2;
+    }
+}
